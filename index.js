f93291c0fbdd4911f1fc0110a7a649764771bfc1
Shane Walsh <studioshane@gmail.com>
2018-10-03 17:43:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,15 +68,11 @@ return currentCart.reduce(getSum)
 
 
 function removeFromCart(item) {
-  for (var i = 0; i < cart.length; i++) {
+  for (var i = 0; i < cart.length; i++) 
     if (cart[i].itemName==item) {
       cart.splice(i, 1);
-      
       return cart;}
-  
   {return ("That item is not in your cart.")}
-}
-  
 }
 
 
